b85b1fe03995dd194b228e1968fb5c1fb1f6614b
sxnazirov <s.x.nazirov@gmail.com>
2022-01-23 07:49:51 +0500

    native vue version

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="app">
+    <div class="post" v-for="post in posts" :key="post.id">
+      <h2>{{post.title}}</h2>
+      <p>{{post.body}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return {
+      posts: []
+    }
+  },
+  async mounted() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=3')
+    const posts = await res.json()
+    this.posts = posts
+  }
 }
 </script>
 
@@ -16,6 +30,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  width: 400px;
+}
+.post {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
 }
 </style>
